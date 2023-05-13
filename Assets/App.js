191af540ce13100b611bd6a29a1f309a4b0393e3
Fiza-Sahar <fizasahar66@gmail.com>
2023-05-14 01:10:13 +0500

    App.js

---
+++ Assets/App.js
@@ -0,0 +1,19 @@
+//Question_01:
+var_1 , var_2 , var_3 = "fiza" , 10 , "Hello world";
+
+//Question_02:
+
+//Legal Variables:
+MyName
+get_amount
+User_Input
+ENTER_Name
+$balance
+
+//illegal Variables:
+99myName (starts with a number)
+my age(contains a space)
+#amount (contains a special character)
+switch (reserved keyword)
+class (reserved keyword)
+my-name (contain -)
